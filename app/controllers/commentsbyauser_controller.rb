26f4bc40befe06034410f5389ddfef271d493e0f
contentinx <317840712@qq.com>
2016-01-16 17:50:33 +0000

    first

--- app/controllers/commentsbyauser_controller.rb
+++ app/controllers/commentsbyauser_controller.rb
@@ -1,18 +1,24 @@
 class CommentsbyauserController < ApplicationController
   def commentsbyauser
-    if(session[:name]==nil)
-      redirect_to :controller=>'login',:action=>'login',:hint=>"请先登陆"
-      return
-    end
+    @hint=params[:hint]
     username=session[:name]
     @comments=Comment.where(:name => username)
   end
   
   def delete
     articlename=params[:aname]
-    username=session[:name]
-    @oldcomment=Comment.find_by_articlename_and_name(articlename,username)
+    #username=session[:name]
+    coursename=params[:cname]
+    commentid=params[:commentid]
+    
+    @oldcomment=Comment.find_by_id(commentid)
     @oldcomment.destroy
+   
+    @article=Article.find_by_articlename(articlename)
+    comments=Comment.where(:coursename =>coursename,:articlename=>articlename)
+      answertimes=comments.size
+      @article.update_attribute(:answertimes,answertimes)
+    
     redirect_to :controller =>'commentsbyauser' ,  :action => 'commentsbyauser'
   end
   
